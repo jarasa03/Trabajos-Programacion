c4c1c1dabf2370fd0b7b28273919632a7168af3a
Javier Arruabarrena Sabroso <145534753+jarasa03@users.noreply.github.com>
2023-10-09 08:44:13 +0200

    Uso de tabuladores

---
+++ UD1_EjerciciosParte1_Ejercicio3.java
@@ -0,0 +1,21 @@
+package unit_1;
+
+// @author jarasa03
+
+public class UD1_EjerciciosParte1_Ejercicio3 {
+
+	public static void main(String[] args) {
+		
+		//Imprimo por pantalla la palabra en español y en inglés separadas con un tabulador, con \t
+		System.out.println("Casa\t\tHouse");
+		System.out.println("Cohete\t\tRocket");
+		System.out.println("Mosca\t\tFly");
+		System.out.println("Ratón\t\tMouse");
+		System.out.println("Teclado\t\tKeyboard");
+		System.out.println("Entrenamiento\tTraining");
+		System.out.println("Pelota\t\tBall");
+		System.out.println("Voleibol\tVolleyball");
+		System.out.println("Pared\t\tWall");
+		System.out.println("Berenjena\tAubergine");
+	}
+}
