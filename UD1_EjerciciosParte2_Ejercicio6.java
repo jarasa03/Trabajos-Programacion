b3623fcf2cf6336dc39ebcff1121541c1d7e8d83
Javier Arruabarrena Sabroso <145534753+jarasa03@users.noreply.github.com>
2023-10-20 16:47:17 +0200

    Operación con números, incluido módulo

--- UD1_EjerciciosParte2_Ejercicio6.java
+++ UD1_EjerciciosParte2_Ejercicio6.java
@@ -2,37 +2,52 @@ package unit_1;
 
 // @author jarasa03
 
-import java.util.Scanner;
-
 public class UD1_EjerciciosParte2_Ejercicio6 {
 
 	public static void main(String[] args) {
 
 		/*
-		 * ENUNCIADO: Crear un programa, que indique si un número es mayor que otro.
-		 * 
+		 * ENUNCIADO: Crear un programa en el que se declaren dos variables de tipo
+		 * entero (int), asignándole valor 20 y 4 respectivamente, y muestre el
+		 * resultado de la suma, resta, multiplicación, división y módulo.
 		 */
 		
-		// Defino como entero los dos números a leer.
-		int num1;
-		int num2;
+		// Defino como enteros dos variables y los diferentes resultados.
+		int var1;
+		int var2;
+		int sum;
+		int res1;
+		int res2;
+		int mul;
+		double div1;
+		double div2;
+		double mod1;
+		double mod2;
 		
-		Scanner sc = new Scanner(System.in);
+		// Defino las variables "var1" y "var2".
+		var1 = 20;
+		var2 = 4;
 		
-		System.out.println("Introducirás dos números y te diré cuál es mayor");
-		System.out.println("Introduce el primer número");
-		num1 = sc.nextInt();
+		// Hago cada una de las operaciones.
+		// El (double) es un casting, es para trasformar una operación en un caso determinado.
+		sum = var1 + var2;
+		res1 = var1 - var2;
+		res2 = var2 - var1;
+		mul = var1 * var2;
+		div1 = var1 / var2;
+		div2 = (double) var2 / var1;
+		mod1 = var1 % var2;
+		mod2 = var2 % var1;
 		
-		System.out.println("Introduce el segundo número");
-		num2 = sc.nextInt();
-		
-		if (num1 > num2) {
-			System.out.println(num1 + " es mayor que " + num2);
-		}
-		else {
-			System.out.println(num2 + " es mayor que " + num1);
-		}
-		
+		// Ahora voy a imprimir por pantalla todos los resultados de cada operación.
+		System.out.println("La suma de " + var1 + " más " + var2 + " es igual a: " + sum);
+		System.out.println("La resta de " + var1 + " menos " + var2 + " es igual a: " + res1);
+		System.out.println("La resta de " + var2 + " menos " + var1 + " es igual a: " + res2);
+		System.out.println("La multiplicación de " + var1 + " por " + var2 + " es igual a: " + mul);
+		System.out.println("La división de " + var1 + " entre " + var2 + " es igual a: " + div1);
+		System.out.println("La división de " + var2 + " entre " + var1 + " es igual a: " + div2);
+		System.out.println("El módulo de " + var1 + " entre " + var2 + " es igual a: " + mod1);
+		System.out.println("El módulo de " + var2 + " entre " + var1 + " es igual a: " + mod2);
 	}
 
 }
