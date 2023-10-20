707dbbf20f4d2019c1b9ba3032ae1359b1dbe07e
Javier Arruabarrena Sabroso <145534753+jarasa03@users.noreply.github.com>
2023-10-20 16:48:24 +0200

    Área de un cuadrado

---
+++ UD1_EjerciciosParte2_Ejercicio8.java
@@ -0,0 +1,22 @@
+package unit_1;
+
+// @author jarasa03
+
+public class UD1_EjerciciosParte2_Ejercicio8 {
+
+	public static void main(String[] args) {
+
+		
+		// ENUNCIADO: Crear un programa que muestre el área de un cuadrado de lado 5.
+		
+		// Defino la variable "lado" como entero y a la vez le doy el valor 5.
+		int lado = 5;
+		
+		// Defino la variable "resul" y le doy el valor de lado * lado.
+		int resul = lado * lado;
+
+		System.out.println("Voy a mostrar el área de un cuadrado de lado = 5");
+		System.out.println("El resultado es: " + resul);
+	}
+
+}
