a07c88766ff5f65273eda127749735c62e87dfb6
Javier Arruabarrena Sabroso <145534753+jarasa03@users.noreply.github.com>
2023-10-20 16:44:20 +0200

    Explicar diferentes tipos de variables

--- UD1_EjerciciosParte2_Ejercicio4.java
+++ UD1_EjerciciosParte2_Ejercicio4.java
@@ -3,33 +3,22 @@ package unit_1;
 // @author jarasa03
 
 public class UD1_EjerciciosParte2_Ejercicio4 {
-	
+
 	public static void main(String[] args) {
-		
-		// Primero defino todas las variables
-		boolean respuesta;
-		int x;
-		int y;
-		int z;
-		double pi;
-		double e;
-		
-		// Imprimo por pantalla el procedimiento a hacer
-		System.out.println("Dado x = 1, y = 4, z = 10, pi = 3.14, e = 2.71");
-		System.out.println("pi * x ^ 2 > y OR 2 * pi * x <= z");
-		
-		// Defino cada variable como el valor indicado en el enunciado
-		x = 1;
-		y = 4;
-		z = 10;
-		pi = 3.14;
-		e = 2.71;
-		
-		// Hago la operación e imprimo por pantalla el resultado
-		respuesta = pi * x * x > y || 2 * pi * x <= z;
-		System.out.println("El resultado de la operación es: " + respuesta);
-		
-		
+
+		/*
+		 * ENUNCIADO: Explica los distintos tipos de variables.
+		 * int, double, long, boolean, byte, char, string
+		 * 
+		 * int: Para números enteros.
+		 * double: Para números con decimales.
+		 * long: Es como un int pero mucho más largo.
+		 * boolean: Es para true or false.
+		 * byte: Para dar a una unidad la unidad de byte.
+		 * char: Es para una sola letra o símbolo.
+		 * string: Es para una cadena de caracteres.
+		 */
+
 	}
 
 }
