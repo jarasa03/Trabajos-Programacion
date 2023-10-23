5096ff01930f53f35d38cb0b5db57a0eb9448a59
Javier Arruabarrena Sabroso <145534753+jarasa03@users.noreply.github.com>
2023-10-23 09:40:43 +0200

    Uso de la librería Math para sin, cos y tan

---
+++ UD1_EjerciciosParte2_Ejercicio13.java
@@ -0,0 +1,33 @@
+package unit_1;
+
+// @author jarasa03
+
+import java.lang.math;
+
+public class UD1_EjerciciosParte2_Ejercicio13 {
+
+	public static void main(String[] args) {
+
+		/*
+		 * ENUNCIADO: Crear un programa que muestre el seno, coseno y tangente de 100.
+		 * (Utilizar la clase Math)
+		 * 
+		 */
+		
+		// Defino la variable pedida en el enunciado.
+		int num = 100;
+		
+		// Hago las operaciones solicitadas.
+		double seno = Math.sin(num);
+		double coseno = Math.cos(num);
+		double tangente = Math.tan(num);
+		
+		// Imprimo las operaciones solicitadas por pantalla junto a una breve explicación.
+		System.out.println("Voy a hacer diferentes operaciones con el número: " + num);
+		System.out.println("El seno de " + num + " es igual a: " + seno);
+		System.out.println("El coseno de " + num + " es igual a: " + coseno);
+		System.out.println("La tangente de " + num + " es igual a: " + tangente);
+		
+	}
+
+}
