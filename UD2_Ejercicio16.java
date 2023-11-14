d6c34360b6b1501e1e461fec51f2b16ad03fd195
Javier Arruabarrena Sabroso <145534753+jarasa03@users.noreply.github.com>
2023-11-14 12:28:31 +0100

    Usando el bucle for mostrar los números pares entre el 1 y el 100

---
+++ UD2_Ejercicio16.java
@@ -0,0 +1,21 @@
+package unit_2;
+
+// @author jarasa03
+
+public class UD2_Ejercicio16 {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Crear un programa que muestre los números pares del 1 al 100 utilizando bucle
+		 * for
+		 */
+
+		System.out.println("Voy a mostrar todos los números pares del 1 al 100");
+		
+		for (int i = 0;(i>=0) && (i<=100);i=i+2) {
+			System.out.println(i);
+		}
+	}
+
+}
