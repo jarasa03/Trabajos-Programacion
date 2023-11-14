d409c3d3066d6239762fb75ed2fd90125e8d6771
Javier Arruabarrena Sabroso <145534753+jarasa03@users.noreply.github.com>
2023-11-14 12:33:04 +0100

    Usando el bucle while mostrar los números pares entre el 1 y el 100

---
+++ UD2_Ejercicio17.java
@@ -0,0 +1,24 @@
+package unit_2;
+
+// @author jarasa03
+
+public class UD2_Ejercicio17 {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Crear un programa que muestre los números pares del 1 al 100 utilizando bucle
+		 * while
+		 */
+
+		int i = 0;
+
+		System.out.println("Voy a mostrar todos los números pares entre 1 y 100");
+		
+		while (i < 100) {
+			i = i + 2;
+			System.out.println(i);
+		}
+	}
+
+}
