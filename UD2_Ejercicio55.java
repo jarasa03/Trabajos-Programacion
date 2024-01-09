b2733de56a066e6ad5382f9949f2d72f13b247e4
Francisco Javier Arruabarrena Sabroso <145534753+jarasa03@users.noreply.github.com>
2024-01-09 12:50:48 +0100

    Array para sumar los positivos y los negativos

--- UD2_Ejercicio55.java
+++ UD2_Ejercicio55.java
@@ -22,19 +22,28 @@ public class UD2_Ejercicio55 {
 		int neg = 0; // Variable par anúmeros negativos.
 
 		// Solicitar los números y rellenar el array.
-		for (int i = 0; i < 8; i++) {
+		for (int i = 0; i < array.length; i++) {
 			System.out.println("Introduce un número entero");
 			num = sc.nextInt();
 			array[i] = num;
-			// Dentro del for, para guardar en dos variables la suma de los negativos y la
-			// suma de los positivos.
-			if (num >= 0) {
-				pos = pos + num;
-			} else {
-				neg = neg + num;
-			}
+//			// Dentro del for, para guardar en dos variables la suma de los negativos y la
+//			// suma de los positivos.
+//			if (num >= 0) {
+//				pos = pos + num;
+//			} else {
+//				neg = neg + num;
+//			}
 		}
 		sc.close();
+
+		// Calculo de la suma de los positivos y los negativos.
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] >= 0) {
+				pos = pos + array[i];
+			} else {
+				neg = neg + array[i];
+			}
+		}
 
 		// Mostrar el array.
 		for (int i = 0; i < array.length; i++) {
