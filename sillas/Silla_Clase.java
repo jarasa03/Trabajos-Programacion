94f3cc8f64f3e9eab819797c1815fdc8b53f4fa9
Francisco Javier Arruabarrena Sabroso <145534753+jarasa03@users.noreply.github.com>
2024-01-15 09:58:37 +0100

    Primer contacto con orientación a objetos

---
+++ sillas/Silla_Clase.java
@@ -0,0 +1,15 @@
+package sillas;
+
+// @author jarasa03
+
+public class Silla_Clase {
+	
+	int patas;
+	String color;
+	int numRuedas;
+	
+	boolean puedeRodar() {
+		return (numRuedas > 0);
+	}
+	}
+
