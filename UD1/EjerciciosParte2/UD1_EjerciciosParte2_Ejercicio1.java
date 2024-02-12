8376890f0dbb8ba249ad9e1332bc7948dabe5682
Francisco Javier Arruabarrena Sabroso <145534753+jarasa03@users.noreply.github.com>
2024-02-12 10:22:53 +0100

    Operación Simple

---
+++ UD1/EjerciciosParte2/UD1_EjerciciosParte2_Ejercicio1.java
@@ -0,0 +1,17 @@
+package unit_1;
+
+// @author jarasa03
+
+public class UD1_EjerciciosParte2_Ejercicio1 {
+	
+	public static void main(String[] args) {
+		
+		int r;
+		
+		System.out.println("Voy a resolver la siguiente operación: ((3 + 2) ^ 2 – 15) / 2 * 5");
+		r = ((3 + 2) ^ 2 - 15) / 2 * 5;
+		System.out.println("La solución es: " + r);
+		
+	}
+
+}
