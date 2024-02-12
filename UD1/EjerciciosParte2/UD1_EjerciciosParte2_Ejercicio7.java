681989ccc5ca3880e74adadc2a3e93b242924e02
Francisco Javier Arruabarrena Sabroso <145534753+jarasa03@users.noreply.github.com>
2024-02-12 18:50:17 +0100

    Dar el num mayor que otro de dos nums introducidos

---
+++ UD1/EjerciciosParte2/UD1_EjerciciosParte2_Ejercicio7.java
@@ -0,0 +1,40 @@
+package unit_1;
+
+// @author jarasa03
+
+import java.util.Scanner;
+
+public class UD1_EjerciciosParte2_Ejercicio7 {
+
+	public static void main(String[] args) {
+
+		/*
+		 * ENUNCIADO: Crear un programa, que indique si un número es mayor que otro.
+		 * 
+		 */
+		
+		// Defino como entero los dos números a leer.
+		int num1;
+		int num2;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introducirás dos números y te diré cuál es mayor");
+		System.out.println("Introduce el primer número");
+		num1 = sc.nextInt();
+		
+		System.out.println("Introduce el segundo número");
+		num2 = sc.nextInt();
+		
+		sc.close();
+		
+		if (num1 > num2) {
+			System.out.println(num1 + " es mayor que " + num2);
+		}
+		else {
+			System.out.println(num2 + " es mayor que " + num1);
+		}
+		
+	}
+
+}
