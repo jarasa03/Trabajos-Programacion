d20e94e4eb0dd668c1990e2aa34fd1ec1a10a131
Francisco Javier Arruabarrena Sabroso <145534753+jarasa03@users.noreply.github.com>
2024-02-12 18:55:19 +0100

    Indicar el error en un código

---
+++ UD1/EjerciciosParte2/UD1_EjerciciosParte2_Ejercicio15.java
@@ -0,0 +1,22 @@
+package unit_1;
+
+// @author jarasa03
+
+public class UD1_EjerciciosParte2_Ejercicio15 {
+
+	public static void main(String[] args) {
+
+		/*
+		 * ENUNCIADO: El siguiente código tiene un error de compilación. Indica cual es:
+		 * int k = 2500; 
+		 * float r = 3.7; 
+		 * byte p = (byte) k;
+		 * 
+		 */
+
+		int k = 2500;
+		float r = 3.7; // No se puede convertir el número de double a float
+		byte p = (byte) k;
+	}
+
+}
