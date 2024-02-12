69c5efb1d0d3c758ae1c60f26ad4a97466340a97
Francisco Javier Arruabarrena Sabroso <145534753+jarasa03@users.noreply.github.com>
2024-02-12 19:03:27 +0100

    Estructura básica y usual de un programa de Java

---
+++ UD1/EjerciciosIniciales/UD1_ejercicio_1.java
@@ -0,0 +1,22 @@
+// Nombre del paquete al que pertenece la clase
+package unit_1;
+
+// Zona de imports
+import static java.lang.System.out; // Esta librería en concreto no es necesario importarla.
+
+//@author jarasa03
+
+// Clase pública, solo una pública por fichero
+public class UD1_ejercicio_1 {
+
+	// Método main
+	public static void main(String[] args) {
+
+		// Declaración de variables
+		String textoBienvenida = "Bienvenido a Java!!!";
+
+		// Sentencias del programa
+		System.out.println(textoBienvenida);
+	}
+
+}
